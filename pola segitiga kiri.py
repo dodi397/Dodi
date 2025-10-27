b7439f976f32719b21182d5728c079234e2c70ec
dodi397 <dodi397dodi@gmail.com>
2025-10-27 13:09:22 +0700

    Add files via upload

---
+++ pola segitiga kiri.py
@@ -0,0 +1,10 @@
+#Pola segitiga kiri
+print("\n2. Pola segitiga tengah:")
+for i in range(1, 6):
+    # Cetak spasi di depan
+    for j in range(5 - i):
+        print(" ", end="")
+    # Cetak bintang
+    for j in range(2 * i - 1):
+        print("*", end="")
+    print()
